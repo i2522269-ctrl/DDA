375588a3060f6f90a42f17ff7d9a5aa3f7ebffcb
i2522269-ctrl <i2522269@continental.edu.pe>
2026-03-29 09:28:27 -0500

    creando la clase de funciones

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("hola Mortales")
+import streamlit as it
+
+class Cliente:
+    def __init__(self, nombre, edad, saldo):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__saldo = saldo
+
+    # Getters (encapsulamiento)
+    def get_nombre(self):
+        return self,__nombre
+
+    def get_edad(self):
+        return self:__edad
+    
+    def get_saldo(self):
+        return self:__saldo
+
+    
+    |# Metodos
+    def depositar_info(self):
+        return f"cliente: {self.__nombre}, edad: {self.__edad}, saldo: {self.__saldo}"
