afc9d737a1c62fa73fcb0ab3caeea46048f0ac6f
i2522269-ctrl <i2522269@continental.edu.pe>
2026-03-29 12:29:45 -0500

    avance en la app 1.2 :)

--- services/cliente_service.py
+++ services/cliente_service.py
@@ -7,6 +7,9 @@ def crear_cliente(nombre, edad, saldo):
     if nombre == "":
         raise ValueError("El nombre es obligatorio")
     
+    if not nombre.replace(" ", "").isalpha():
+        raise ValueError("El nombre debe contener solo letras")
+
     #Validación 2:edad
     if edad < 18:
         raise ValueError("El cliente debe ser mayor de edad")
