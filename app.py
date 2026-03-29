5c8267a1a7570022b555409c55c1a6bc30ba4cc6
i2522269-ctrl <i2522269@continental.edu.pe>
2026-03-29 09:37:50 -0500

    creando la clase de funciones

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as it
 
+#..............................................
+# CLASS (POO)
+#..............................................
+
 class Cliente:
     def __init__(self, nombre, edad, saldo):
         self.__nombre = nombre
@@ -20,3 +24,10 @@ class Cliente:
     |# Metodos
     def depositar_info(self):
         return f"cliente: {self.__nombre}, edad: {self.__edad}, saldo: {self.__saldo}"
+
+#..............................................
+# STREAMLIT
+#..............................................
+
+st.title("Demo POO - Ciencia De Datos")
+st.write("ingrese los datps del cliente")
